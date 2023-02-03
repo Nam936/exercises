c0720b0d1396d4c14f1c4f4e61a5f482e55d265a
NamAnhTH <heopc203@gmail.com>
2023-02-03 17:03:58 +0700

    02.03

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ const swiper = new Swiper('.swiper', {
     grabCursor: true,
     slidesPerView: 3,
     spaceBetween: 30,
-    freeMode: true,
+    freeMode: false,
 });
 
+
+var swiper_2 = new Swiper('.swiper-initialize', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: false,
+    freeMode: true,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    }
+});
