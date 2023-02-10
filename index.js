cb6242dab86fad6176676d18038f019cec320356
NamAnhTH <heopc203@gmail.com>
2023-02-10 17:14:16 +0700

    fix feedback

--- index.js
+++ index.js
@@ -1,39 +1,54 @@
 const swiper = new Swiper('.swiper', {
-
     pagination: {
         el: '.swiper-pagination',
     },
     grabCursor: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
-    // breakpoints:{
-    //     600:{
-    //         slidesPerView: 1
-    //     },
-    //     800:{
-    //         slidesPerView:1
-    //     },
-    // }
+    breakpoints:{
+        768:{
+            slidesPerView:2,
+            
+        },
+        1024:{
+            slidesPerView:3,
+            
+        },
+    }
+    
 });
 
 
 var swiper_2 = new Swiper('.swiper-initialize', {
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 30,
     loop: false,
-    freeMode: true,
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    // breakpoints:{
-    //     600: {
-    //         slidesPerView: 1
-    //     },
-    //     800: {
-    //         slidesPerView:1
-    //     }
-    // }
+    breakpoints:{
+        768:{
+            slidesPerView:2,
+            
+        },
+        1024:{
+            slidesPerView:3,
+            
+        },
+    }
 });
 
+
+
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('.header').addClass('sticky');
+        } else {
+            $('.header').removeClass('sticky');
+        }
+    })
+});
 
